3678fd4944ebe6ca9eec83c07aad10ffaf33dadf
MilerSoares <milersoares@yahoo.com.br>
2023-12-06 22:11:06 -0300

    Mudanças a serem submetidas: 	modified:   work/app/composition/header.php 	new file:   work/app/styles/id/header.css 	modified:   work/app/styles/id/register.css 	new file:   work/app/styles/paginas/home.css 	new file:   work/app/styles/paginas/intro.css 	new file:   work/app/styles/paginas/traffic.css 	modified:   work/app/views/home.php 	new file:   work/app/views/introducao.php 	modified:   work/app/views/transition/traffic.php

--- work/app/views/home.php
+++ work/app/views/home.php
@@ -2,17 +2,28 @@
     include_once("../composition/header.php"); 
 ?>
 
-<article>
-    <div class="btns-sect text-center d-flex justify-content-center align-items-center vh-100">
-        <div class="d-grid gap-2 small">
-            <h6> Bem vindo! <?php echo $nomeUsuario; ?></h6>
-            <button class="btn btn-primary" type="button" onclick="navigation.movimentacaoProdutos()">Cadastro</button>
-            <button class="btn btn-primary" type="button" onclick="navigation.gerenciamentoDados()">Gerenciamento de Dados</button>
-        </div>
-    </div>
-</article>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PLG LOG - Home</title>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    <link rel="stylesheet" href="/styles/paginas/home.css">
+</head>
+<body>    
+    
+    <article>
+        <div class="btn-home">
+            <div class="d-grid">
+                <h6> Bem vindo! <?php echo $nomeUsuario; ?></h6>
+                <button class="btn btn-primary" type="button" onclick="navigation.movimentacaoProdutos()">Cadastro</button>
+                <button class="btn btn-primary" type="button" onclick="navigation.gerenciamentoDados()">Gerenciamento de Dados</button>
+            </div>
+            </div>
+        </article>
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous">
 </script>
