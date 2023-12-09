4014e1628c44412fbf03506884ed93f9e8f4ccbd
MilerSoares <milersoares@yahoo.com.br>
2023-12-09 00:20:49 -0300

    FlexBox_Traffic

--- work/app/views/transition/traffic.php
+++ work/app/views/transition/traffic.php
@@ -2,7 +2,18 @@
 include_once("../../composition/header.php");
 ?>
 
-<link rel="stylesheet" href="/styles/paginas/traffic.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" href="/styles/paginas/traffic.css">
+
+</head>
+<body>
+    
+
 
 <article id="selectionTraffic">
     <div>
@@ -17,47 +28,47 @@ include_once("../../composition/header.php");
 
 
 <div class="page container-sm" id='entrySection' style="display: none;">
-<!-- <div class="formulario"> -->
+
     <form method="POST" class="formCadastro" action="/controllers/classSupplier.php">
         <h1>Cadastro Nota Fiscal</h1>
         <!-- Nota Fiscal -->
       
-            <input type="text" class="nf" id="inputinvoice" placeholder="Nota Fiscal" name="invoice" maxlength="9" required>
+            <input type="text" id="inputinvoice" placeholder="Nota Fiscal" name="invoice" maxlength="9" required>
             <label for="inputinvoice"></label>
  
         <!-- Data de Emissão -->
-            <input type="date" class="de" id="inputDataEmissao" placeholder="Data de Emissão" name="dataEmissao" required>
+            <input type="date" id="inputDataEmissao" placeholder="Data de Emissão" name="dataEmissao" required>
             <label for="inputDataEmissao"></label>
  
 
         <!-- Nome do Fornecedor -->
-            <input type="text" class="fornecedor" id="inputNomeFornecedor" placeholder="Nome do Fornecedor" name="nomeFornecedor" required>
+            <input type="text" id="inputNomeFornecedor" placeholder="Nome do Fornecedor" name="nomeFornecedor" required>
             <label for="inputNomeFornecedor"></label>
  
         <!-- CNPJ do Fornecedor -->
-            <input type="text" class="cnpj" id="inputCnpjFornecedor" placeholder="CNPJ do Fornecedor" name="cnpjFornecedor" maxlength="14" required>
+            <input type="text" id="inputCnpjFornecedor" placeholder="CNPJ do Fornecedor" name="cnpjFornecedor" maxlength="14" required>
             <label for="inputCnpjFornecedor"></label>
        
         <!-- Logradouro -->
-            <input type="text" class="end" id="inputLogradouro" placeholder="Logradouro" name="Logradouro" required>
+            <input type="text" id="inputLogradouro" placeholder="Logradouro" name="Logradouro" required>
             <label for="inputLogradouro"></label>
  
         <!-- Detalhes do Endereço -->
-            <input type="text" class="cidade" id="inputCidade" placeholder="Cidade" name="cidade" required>
+            <input type="text" id="inputCidade" placeholder="Cidade" name="cidade" required>
             <label for="inputCidade"></label>
 
  
-            <input type="text" class="uf" id="inputEstado" placeholder="Estado" name="estado" required>
+            <input type="text" id="inputEstado" placeholder="Estado" name="estado" required>
             <label for="inputEstado"></label>
    
  
-            <input type="text" class="cep" id="inputCEP" placeholder="CEP" name="cep" maxlength="8" required>
+            <input type="text" id="inputCEP" placeholder="CEP" name="cep" maxlength="8" required>
             <label for="inputCEP"></label>
  
         
-            <input type="submit" value="Proximo" class="btn btn-danger btn-lg" id="cadastro" />
+            <input type="submit" value="Proximo" class="prox btn-danger btn-lg" id="cadastro" />
     </form>
-    <!-- </div>     -->
+
 </div>
  
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
