c1bf8594cc8c47b3fead695b8a1f1c410918d515
MilerSoares <milersoares@yahoo.com.br>
2023-12-09 00:00:37 -0300

    No ramo Plg_Log_remaster-Miler  Your branch is up to date with 'origin/Plg_Log_remaster-Miler'.
     Mudanças a serem submetidas:
    	modified:   work/app/styles/paginas/traffic.css
    	modified:   work/app/views/register/registerItem.php

--- work/app/views/register/registerItem.php
+++ work/app/views/register/registerItem.php
@@ -75,7 +75,7 @@
         <div class="d-grid gap-2 d-md-block text-center d-flex justify-content-center align-items-center">
             <input type="submit" value="Cadastrar" class="btn btn-danger btn-lg" id="cadastro" />
             <input type="button" value="Calcular" onclick="calcPrice()" class="btn btn-danger btn-lg" id="calcular" />
-            <a href="./register.php">
+            <a href="register.php">
                 <button type="button" class="btn btn-danger btn-lg">Voltar</button>
             </div>
     </form>
